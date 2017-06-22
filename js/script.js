061e52753034dee24b38dca0d960631cc78c21fa
Ali-Kiyan <Alikiyand@gmail.com>
2017-06-22 10:49:17 +0100

    adding my first php project

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // Modal Image Gallery
 function onClick(element) {
+  if (document.getElementById("img01")){
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
+  }
+  if (document.getElementById("img02"))
+  {
+    document.getElementById("img02").src = element.src;
     document.getElementById("modal02").style.display = "block";
+
+  }
     var captionText = document.getElementById("caption");
     captionText.innerHTML = element.alt;
 }
