0a119853662b9a1f8b1c9dd7bef81b248c79deec
Ali <alikiyand@gmail.com>
2017-09-30 23:57:32 +0100

    nav refactor

--- js/script.js
+++ js/script.js
@@ -7,6 +7,29 @@ function onClick(element) {
 }
 
 
+// sticky nav functionality
+
+jQuery(document).ready(function(){
+	if( $('.cd-stretchy-nav').length > 0 ) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+
+		stretchyNavs.each(function(){
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+
+			stretchyNavTrigger.on('click', function(event){
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
+
+		$(document).on('click', function(event){
+			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+		});
+	}
+});
+
+
 
 // Change style of navbar on scroll
 // window.onscroll = function() {myFunction()};
