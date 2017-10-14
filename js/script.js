2b028a0ec90f55db24aab0ae6e220f35425ffb38
Ali <alikiyand@gmail.com>
2017-10-14 20:21:54 +0100

    adding the skeleton

--- js/script.js
+++ js/script.js
@@ -8,6 +8,31 @@ $(document).ready(function() {
 });
 
 
+//Owl initializer funciton to prepare the carousel
+
+
+$(document).ready(function(){
+
+  $(".owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  });
+
+});
+
+
 
 
 
